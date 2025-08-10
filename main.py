23c08e3154cffb8fea522c5943dc7304a158f4d1
kanakAutomatizacion <kanakrpa@gmail.com>
2025-08-10 09:45:30 -0500

    Ajuste

--- main.py
+++ main.py
@@ -6,10 +6,7 @@ app = FastAPI()
 
 @app.post("/completar-cuenta")
 async def completar_cuenta(request: Request):
-    raw_data = await request.json()
-    
-    # Si viene con el formato de Automate, extraemos el body real
-    data = raw_data.get("body", raw_data)
+    data = await request.json()
     
     nit_vendedor = data.get("factura", {}).get("Nit_Vendedor", "")
     cuenta_asignada = REGLAS_CUENTA_CONTABLE.get(nit_vendedor, CUENTA_POR_DEFECTO)
