06158fc1d11bdeeea7b0ee3403eb83e4d478ca1c
kanakAutomatizacion <kanakrpa@gmail.com>
2025-08-10 09:04:15 -0500

    Ajuste para leer body desde Power Automate

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ app = FastAPI()
 
 @app.post("/completar-cuenta")
 async def completar_cuenta(request: Request):
-    data = await request.json()
+    raw_data = await request.json()
+    
+    # Si viene con el formato de Automate, extraemos el body real
+    data = raw_data.get("body", raw_data)
     
     nit_vendedor = data.get("factura", {}).get("Nit_Vendedor", "")
     cuenta_asignada = REGLAS_CUENTA_CONTABLE.get(nit_vendedor, CUENTA_POR_DEFECTO)
